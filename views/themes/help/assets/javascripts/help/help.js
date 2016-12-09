91aa6a308231b155690ace652cad533b2f9e9462
jason weng <wengjianm@gmail.com>
2016-12-09 17:57:46 +0800

    feat(help) add help list and search func@2

--- views/themes/help/assets/javascripts/help/help.js
+++ views/themes/help/assets/javascripts/help/help.js
@@ -34,67 +34,87 @@
 
         bind: function() {
             this.$element
-                .on(EVENT_CLICK, '.qor-help__lists a', this.loadDoc)
-                .on(EVENT_KEYUP, '.qor-help__search', this.search);
+                .on(EVENT_CLICK, '.qor-help__lists [data-inline-url]', this.loadDoc)
+                .on(EVENT_KEYUP, '.qor-help__search', this.searchKeyup.bind(this))
+                .on(EVENT_CLICK, '.qor-help__search-button', this.search.bind(this))
+                .on(EVENT_CLICK, '.qor-doc__close', this.closeDoc);
         },
 
         unbind: function() {
             this.$element
-                .off(EVENT_CLICK, '.qor-help__lists a', this.loadDoc)
-                .off(EVENT_KEYUP, '.qor-help__search', this.search);
+                .off(EVENT_CLICK, '.qor-help__lists [data-inline-url]', this.loadDoc)
+                .off(EVENT_KEYUP, '.qor-help__search', this.searchKeyup.bind(this))
+                .off(EVENT_CLICK, '.qor-help__search-button', this.search.bind(this))
+                .off(EVENT_CLICK, '.qor-doc__close', this.closeDoc);
         },
 
-        search: function(e) {
+        searchKeyup: function(e) {
             if (e.keyCode == 13) {
-                var $category = $('.qor-help__search-category'),
-                    $input = $('.qor-help__search'),
-                    $list = $('.qor-help__lists'),
-                    $loading = $(QorHelpDocument.TEMPLATE_LOADING),
-                    url = [
-                        $(this).data().helpFilterUrl,
-                        '?',
-                        $category.prop('name'),
-                        '=',
-                        $category.find('option:selected').text(),
-                        '&',
-                        $input.prop('name'),
-                        '=',
-                        $input.val()
-                    ].join('');
-
-                $.ajax(url, {
-                    method: 'GET',
-                    dataType: 'html',
-                    processData: false,
-                    contentType: false,
-                    beforeSend: function() {
-                        $list.hide().after($loading);
-                        window.componentHandler.upgradeElement($loading.children()[0]);
-                    },
-
-                    success: function(html) {
-                        $list.html($(html).find('.qor-help__lists').html()).show();
-                        $loading.remove();
-                    },
-                    error: function(xhr, textStatus, errorThrown) {
-                        $list.show();
-                        $loading.remove();
-                        window.alert([textStatus, errorThrown].join(': '));
-                    }
-                });
-
+                this.searchAction();
             }
+        },
+
+        search: function() {
+            this.searchAction();
+        },
+
+        searchAction: function() {
+            var $category = $('.qor-help__search-category'),
+                $input = $('.qor-help__search'),
+                $list = $('.qor-help__lists'),
+                $loading = $(QorHelpDocument.TEMPLATE_LOADING),
+                url = [
+                    $(this).data().helpFilterUrl,
+                    '?',
+                    $category.prop('name'),
+                    '=',
+                    $category.find('option:selected').text(),
+                    '&',
+                    $input.prop('name'),
+                    '=',
+                    $input.val()
+                ].join('');
+
+            $.ajax(url, {
+                method: 'GET',
+                dataType: 'html',
+                processData: false,
+                contentType: false,
+                beforeSend: function() {
+                    $list.hide().after($loading);
+                    window.componentHandler.upgradeElement($loading.children()[0]);
+                },
+
+                success: function(html) {
+                    $list.html($(html).find('.qor-help__lists').html()).show();
+                    $loading.remove();
+                },
+                error: function(xhr, textStatus, errorThrown) {
+                    $list.show();
+                    $loading.remove();
+                    window.alert([textStatus, errorThrown].join(': '));
+                }
+            });
         },
 
         loadDoc: function(e) {
             var $element = $(e.target),
+                $lis = $('.qor-help__lists li'),
                 $li = $element.closest('li'),
                 $preview = $li.find('.qor-doc__preview'),
                 $loading = $(QorHelpDocument.TEMPLATE_LOADING),
                 url = $element.data().inlineUrl;
 
+
+            $lis.not($li).hide();
+
             if ($preview.size()) {
-                $preview.toggle();
+                if ($preview.is(':visible')) {
+                    $preview.hide();
+                    $lis.show();
+                } else {
+                    $preview.show();
+                }
                 return false;
             }
 
@@ -108,7 +128,7 @@
                     window.componentHandler.upgradeElement($loading.children()[0]);
                 },
                 success: function(html) {
-                    $($(html).find('.qor-form-container').html()).appendTo($li).addClass('qor-fieldset qor-doc__preview');
+                    $(html).find('.qor-form-container').appendTo($li).addClass('qor-fieldset qor-doc__preview').prepend(QorHelpDocument.TEMPLATE_PREVIEW_CLOSE);
                     $loading.remove();
                 },
                 error: function(xhr, textStatus, errorThrown) {
@@ -120,6 +140,14 @@
             return false;
         },
 
+        closeDoc: function() {
+            var $lis = $('.qor-help__lists li'),
+                $preview = $('.qor-doc__preview');
+
+            $lis.show();
+            $preview.hide();
+        },
+
         destroy: function() {
             this.unbind();
             this.$element.removeData(NAMESPACE);
@@ -127,6 +155,7 @@
     };
 
     QorHelpDocument.TEMPLATE_LOADING = '<div style="text-align: center; margin-top: 30px;"><div class="mdl-spinner mdl-js-spinner is-active qor-layout__bottomsheet-spinner"></div></div>';
+    QorHelpDocument.TEMPLATE_PREVIEW_CLOSE = '<a href="javascript://" class="mdl-button mdl-button--fab mdl-button--primary mdl-js-button mdl-js-ripple-effect qor-doc__close"><i class="material-icons">close</i></a>';
 
     QorHelpDocument.plugin = function(options) {
         return this.each(function() {
