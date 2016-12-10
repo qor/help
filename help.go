62e40c67383eaac081a8f703ca84156f211e8ae7
Jinzhu <wosmvp@gmail.com>
2016-12-10 20:00:46 +0800

    Refactor categories

--- help.go
+++ help.go
@@ -4,6 +4,7 @@ import (
 	"database/sql/driver"
 	"errors"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/jinzhu/gorm"
@@ -28,18 +29,27 @@ func (categories *Categories) Scan(data interface{}) (err error) {
 	switch values := data.(type) {
 	case []byte:
 		if string(values) != "" {
-			var cs []string
+			var strs []string
 			for _, c := range strings.Split(string(values), ";") {
-				cs = append(cs, strings.TrimFunc(c, func(v rune) bool {
-					return string(v) == "[" || string(v) == "]"
+				strs = append(strs, strings.TrimFunc(c, func(v rune) bool {
+					r := strings.TrimSpace(string(v))
+					return r == "" || r == "[" || r == "]"
 				}))
 			}
-			categories.Scan(cs)
+			categories.Scan(strs)
 		}
 	case string:
 		return categories.Scan([]byte(values))
 	case []string:
-		categories.Categories = values
+		var strs []string
+		for _, v := range values {
+			if strings.TrimSpace(v) != "" {
+				strs = append(strs, v)
+			}
+		}
+		sort.Strings(strs)
+
+		categories.Categories = strs
 	default:
 		err = errors.New("unsupported driver -> Scan pair for Categories")
 	}
@@ -75,6 +85,11 @@ func (qorHelpEntry *QorHelpEntry) ConfigureQorResource(res resource.Resourcer) {
 			res.Meta(&admin.Meta{
 				Name: "Categories",
 				Valuer: func(record interface{}, context *qor.Context) interface{} {
+					if field, ok := context.GetDB().NewScope(record).FieldByName("Categories"); ok {
+						if categories, ok := field.Field.Interface().(Categories); ok {
+							return categories.Categories
+						}
+					}
 					return []string{}
 				},
 				Config: &admin.SelectManyConfig{Collection: func(record interface{}, context *qor.Context) [][]string {
